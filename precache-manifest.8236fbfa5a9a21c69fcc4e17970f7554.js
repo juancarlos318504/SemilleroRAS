ec10dc51489acae64fc9b97876cedc9f954394c8
Juan Carlos Cuasquen <juan.cuasquen@uao.edu.co>
2020-10-25 18:34:48 -0500

    Updates

---
+++ precache-manifest.8236fbfa5a9a21c69fcc4e17970f7554.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6988559ffc159fa89a90e749ab1d20e9",
+    "url": "/SemilleroRAS/index.html"
+  },
+  {
+    "revision": "d1714a01df71b8629744",
+    "url": "/SemilleroRAS/static/css/main.6a6b2288.chunk.css"
+  },
+  {
+    "revision": "e9e5b079b212bd41bf8b",
+    "url": "/SemilleroRAS/static/js/2.750cd9e9.chunk.js"
+  },
+  {
+    "revision": "27df78d2822439c2fc110c0a385025d0",
+    "url": "/SemilleroRAS/static/js/2.750cd9e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1714a01df71b8629744",
+    "url": "/SemilleroRAS/static/js/main.c0fb2720.chunk.js"
+  },
+  {
+    "revision": "bb50cbad4844923b3b73",
+    "url": "/SemilleroRAS/static/js/runtime-main.aef47f33.js"
+  },
+  {
+    "revision": "097a36f2b08da9181cf64e297b1cddde",
+    "url": "/SemilleroRAS/static/media/LogoSIR.097a36f2.svg"
+  },
+  {
+    "revision": "9c698f836c6ce6a32f0f329bb8cacf8e",
+    "url": "/SemilleroRAS/static/media/Logo_UAO.9c698f83.svg"
+  },
+  {
+    "revision": "0bc629aa9bee202a2d8890a01be0664a",
+    "url": "/SemilleroRAS/static/media/videobg.0bc629aa.mp4"
+  }
+]);
